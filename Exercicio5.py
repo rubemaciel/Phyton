e0e9bebf4768f1436f5272ce9a2e04ba8de36a67
rubemaciel <rubens.maciel@aluno.impacta.edu.br>
2026-08-27 16:01:01 -0300

    Add conversion from meters to centimeters and millimeters

---
+++ Exercicio5.py
@@ -0,0 +1,6 @@
+conversãoMedidas = float(input("Digite valor quantos metros deseja converter:"))
+cen = conversãoMedidas * 100
+mil = conversãoMedidas * 1000
+
+print(f"Centímetros:{cen}\n"
+      f"Milímetros:{mil}")
