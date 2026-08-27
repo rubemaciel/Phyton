7947e7918719b93f73a45aecbed49597be32ff55
rubemaciel <rubens.maciel@aluno.impacta.edu.br>
2026-08-27 16:00:31 -0300

    Add calculations for double, triple, and half of a value

---
+++ exercicio4.py
@@ -0,0 +1,9 @@
+Valor = float(input("Digite um valor:"))
+
+dobroValor = Valor * 2
+triplo = Valor * 3 
+metade = Valor/2
+
+print(f"Dobro:{dobroValor}\n"
+      f"Triplo:{triplo}\n"
+      f"Metade:{metade}")
