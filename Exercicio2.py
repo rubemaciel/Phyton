31c9bc77b19e1188943c5cb4d6db59d476cefdcc
rubemaciel <rubens.maciel@aluno.impacta.edu.br>
2026-08-27 15:59:25 -0300

    Create Exercicio2.py

---
+++ Exercicio2.py
@@ -0,0 +1,7 @@
+#Recebimento de entrada, fornecido pelo usuario
+pValor = float(input("Digite primeiro valor:"))
+sValor = float(input("Digite segundo valor:"))
+#Calculo da média somando valores dos números, dividindo pela quantidade de números 
+media = (pValor + sValor)/2
+#Resultado final
+print(f"Valor da media é {media}")
